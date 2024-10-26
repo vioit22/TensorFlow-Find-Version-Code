24ec2c90a3c6d27833d37220f6a51f093e0d8b61
Vio IT <150695785+vioit22@users.noreply.github.com>
2024-10-26 23:10:39 +0300

    Add files via upload

---
+++ Tensorflow Show Number Version.py
@@ -0,0 +1,5 @@
+root= input("Press Enter the show TensorFlow Number Version")
+import tensorflow as tf
+print("The TensorFlow latest Version is:")
+print(tf.__version__)
+root= input("Press Enter to exit! Developed by Vio IT!")
